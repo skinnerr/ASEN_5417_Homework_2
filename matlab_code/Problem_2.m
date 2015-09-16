f30fb617f262053a8af719c6533248f284834ebb
Ryan Skinner <anomaly1452@gmail.com>
2015-09-16 14:03:01 -0600

    Mid-way through write-up for Problem 3.

--- matlab_code/Problem_2.m
+++ matlab_code/Problem_2.m
@@ -39,7 +39,7 @@ function [] = Problem_2()
     rk2.uou0 = rk2.fp;
     rk2.vou0 = rk2.eta .* rk2.fp - 0.5 * rk2.f;
     
-    % Repeat integration using a higher-order method (here, ode45).
+    % Repeat integration using a higher-order method (ode45).
     [T, Y] = ode45(@stream, [0,4], [0,1,0]);
     o45.eta = T;
     o45.f   = Y(:,1);
@@ -48,27 +48,26 @@ function [] = Problem_2()
     o45.uou0 = o45.fp;
     o45.vou0 = o45.eta .* o45.fp - 0.5 * o45.f;
     
-    
     % Calculate solution using best-fit expression.
-    best.eta = rk2.eta;
-    best.f   = 1.07313 * erf(0.825833 * best.eta);
-    best.fp  =                     exp(-0.682*rk2.eta.^2);
-    best.fpp = -0.682 * rk2.eta .* exp(-0.682*rk2.eta.^2);
-    best.uou0 = best.fp;
-    best.vou0 = best.eta .* best.fp - 0.5 * best.f;
+    bst.eta = rk2.eta;
+    bst.f   = 1.07313 * erf(0.825833 * bst.eta);
+    bst.fp  =                     exp(-0.682*bst.eta.^2);
+    bst.fpp = -0.682 * bst.eta .* exp(-0.682*bst.eta.^2);
+    bst.uou0 = bst.fp;
+    bst.vou0 = bst.eta .* bst.fp - 0.5 * bst.f;
     
     % Plot fields.
     figure();
     hold on;
-    plot(rk2.eta,  rk2.uou0,        'DisplayName', 'U/U_0 (RK2)');
-    plot(o45.eta,  o45.uou0,  '--', 'DisplayName', 'U/U_0 (ode45)');
-    plot(best.eta, best.uou0, '-.', 'DisplayName', 'U/U_0 (Best Fit)');
-    plot(rk2.eta,  rk2.vou0,        'DisplayName', 'V/U_0 (RK2)');
-    plot(o45.eta,  o45.vou0,  '--', 'DisplayName', 'V/U_0 (ode45)');
-    plot(best.eta, best.vou0, '-.', 'DisplayName', 'V/U_0 (Best Fit)');
-    plot(rk2.eta,  rk2.f,           'DisplayName', 'f (RK2)');
-    plot(o45.eta,  o45.f,     '--', 'DisplayName', 'f (ode45)');
-    plot(best.eta, best.f,    '-.', 'DisplayName', 'f (Best Fit)');
+    plot(rk2.eta, rk2.uou0,       'DisplayName', 'U/U_0 (RK2)');
+    plot(o45.eta, o45.uou0, '--', 'DisplayName', 'U/U_0 (ode45)');
+    plot(bst.eta, bst.uou0, '-.', 'DisplayName', 'U/U_0 (Best Fit)');
+    plot(rk2.eta, rk2.vou0,       'DisplayName', 'V/U_0 (RK2)');
+    plot(o45.eta, o45.vou0, '--', 'DisplayName', 'V/U_0 (ode45)');
+    plot(bst.eta, bst.vou0, '-.', 'DisplayName', 'V/U_0 (Best Fit)');
+    plot(rk2.eta, rk2.f,          'DisplayName', 'f (RK2)');
+    plot(o45.eta, o45.f,    '--', 'DisplayName', 'f (ode45)');
+    plot(bst.eta, bst.f,    '-.', 'DisplayName', 'f (Best Fit)');
     hleg = legend('show');
     set(hleg, 'location', 'southwest');
     xlim([eta0,etaf]);
@@ -82,8 +81,3 @@ function dy = stream(~, y)
     dy(2) = y(3);
     dy(3) = -y(1)*y(2);
 end
-
-
-
-
-
--- matlab_code/Problem_3.m
+++ matlab_code/Problem_3.m
@@ -7,16 +7,11 @@ function [] = Problem_3()
     
     Set_Default_Plot_Properties();
 
-    % Initialize times at which to evaluate solution.
+    % Initialize times at which to evaluate solution (with dt = hmax*2).
     t0 = 0;
-    tf = 10;
-    dt = 0.5;
+    tf = 15;
+    dt = 8/19;
     t  = (t0:dt:tf)';
-    
-    % Calculate real values analytically.
-    ya   = (1/2) * exp(-19*t/2) - (19/2) * exp(-t/2);
-    ypa  = (19/4) * (exp(-t/2) - exp(-19*t/2));
-    yppa = (19/4) * ((-1/2)*exp(-t/2) - (-19/2)*exp(-19*t/2));
     
     % Initialize velocities and positions.
     y   = zeros(length(t),1);
@@ -47,19 +42,74 @@ function [] = Problem_3()
         yp(n+1)  = ypn  + l1/6 + l2/3 + l3/3 + l4/6;
         ypp(n+1) = yppn + m1/6 + m2/3 + m3/3 + m4/6;
     end
+    hmt2.t = t;
+    hmt2.y = y;
+    hmt2.y10 = interp1(hmt2.t,hmt2.y,10);
+
+    % Initialize times at which to evaluate solution (with dt = hmax/2).
+    t0 = 0;
+    tf = 15;
+    dt = 2/19;
+    t  = (t0:dt:tf)';
+    
+    % Initialize velocities and positions.
+    y   = zeros(length(t),1);
+    yp  = zeros(length(t),1);
+    ypp = zeros(length(t),1);
+    y(1)   = -9;
+    yp(1)  = 0;
+    ypp(1) = 42.75;
+    
+    % Perform integration using an RK4 method.
+    for n = 1:(length(t)-1)
+        yn   = y(n);    % k
+        ypn  = yp(n);   % l
+        yppn = ypp(n);  % m
+        k1 = dt * ypn;
+        l1 = dt * yppn;
+        m1 = dt * -(19/4)*yn - 10*ypn;
+        k2 = dt * (ypn + l1/2);
+        l2 = dt * (yppn + m1/2);
+        m2 = dt * -(19/4)*(yn + k1/2) - 10*(ypn + l1/2);
+        k3 = dt * (ypn + l2/2);
+        l3 = dt * (yppn + m2/2);
+        m3 = dt * -(19/4)*(yn + k2/2) - 10*(ypn + l2/2);
+        k4 = dt * (ypn + l3);
+        l4 = dt * (yppn + m3);
+        m4 = dt * -(19/4)*(yn + k3) - 10*(ypn + l3);
+        y(n+1)   = yn   + k1/6 + k2/3 + k3/3 + k4/6;
+        yp(n+1)  = ypn  + l1/6 + l2/3 + l3/3 + l4/6;
+        ypp(n+1) = yppn + m1/6 + m2/3 + m3/3 + m4/6;
+    end
+    hmd2.t = t;
+    hmd2.y = y;
+    hmd2.y10 = interp1(hmd2.t,hmd2.y,10);
+    
+    % Calculate solution analytically.
+    ya = (1/2) * exp(-19*hmd2.t/2) - (19/2) * exp(-hmd2.t/2);
+    ya10 = (1/2) * exp(-19*10/2) - (19/2) * exp(-10/2);
     
     % Plot fields.
     figure();
     hold on;
-%     plot(t,yppa,'DisplayName','y'''' analytic');
-%     plot(t,ypa ,'DisplayName','y'' analytic');
-    plot(t,ya  ,'DisplayName','y analytic');
-%     plot(t,ypp ,'DisplayName','y''''');
-%     plot(t,yp  ,'DisplayName','y''');
-    plot(t,y   ,'DisplayName','y');
+    plot(hmd2.t,    ya,     'DisplayName','analytic');
+    plot(hmt2.t,hmt2.y,'--','DisplayName','RK4 (hmax*2)');
+    plot(hmd2.t,hmd2.y,'-.','DisplayName','RK4 (hmax/2)');
     hleg = legend('show');
-    set(hleg, 'location', 'northeast');
-    xlim([t0,tf]);
+    set(hleg, 'location', 'eastoutside');
+    xlim([0,10]);
     xlabel('\eta');
+    ylabel('y');
+    
+    % Print statistics.
+    fprintf('Values of y(10) for each method:\n');
+    fprintf('RK4 (hmax/2): %.2f\n',hmd2.y10);
+    fprintf('RK4 (hmax*2): %.2f\n',hmt2.y10);
+    fprintf('Actual      : %.2f\n',ya10);
     
 end
+
+
+
+
+
