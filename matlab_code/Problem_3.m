6027d7836dc46d82d1d1cd358077b0cdcfa4f686
Ryan Skinner <anomaly1452@gmail.com>
2015-09-16 21:24:20 -0600

    Code and assignment completed; all correct.

--- matlab_code/Problem_3.m
+++ matlab_code/Problem_3.m
@@ -63,19 +63,15 @@ function [] = Problem_3()
         yppn = ypp(n);  % m
         k1 = dt * ypn;
         l1 = dt * yppn;
-        m1 = dt * -(19/4)*yn - 10*ypn;
-        k2 = dt * (ypn + l1/2);
-        l2 = dt * (yppn + m1/2);
-        m2 = dt * -(19/4)*(yn + k1/2) - 10*(ypn + l1/2);
-        k3 = dt * (ypn + l2/2);
-        l3 = dt * (yppn + m2/2);
-        m3 = dt * -(19/4)*(yn + k2/2) - 10*(ypn + l2/2);
-        k4 = dt * (ypn + l3);
-        l4 = dt * (yppn + m3);
-        m4 = dt * -(19/4)*(yn + k3) - 10*(ypn + l3);
+        k2 = dt * (ypn + k1/2);
+        l2 = dt * (yppn + l1/2);
+        k3 = dt * (ypn + k2/2);
+        l3 = dt * (yppn + l2/2);
+        k4 = dt * (ypn + k3);
+        l4 = dt * (yppn + l3);
         y(n+1)   = yn   + k1/6 + k2/3 + k3/3 + k4/6;
         yp(n+1)  = ypn  + l1/6 + l2/3 + l3/3 + l4/6;
-        ypp(n+1) = yppn + m1/6 + m2/3 + m3/3 + m4/6;
+        ypp(n+1) = (-19/4) * y(n+1) - 10 * yp(n+1);
     end
     hmd2.t = t;
     hmd2.y = y;
@@ -100,9 +96,9 @@ function [] = Problem_3()
     
     % Print statistics.
     fprintf('Values of y(10) for each method:\n');
-    fprintf('RK4 (hmax/2): %.2f\n',hmd2.y10);
-    fprintf('RK4 (hmax*2): %.2f\n',hmt2.y10);
-    fprintf('Actual      : %.2f\n',ya10);
+    fprintf('Actual      : %.4f\n',ya10);
+    fprintf('RK4 (hmax/2): %.4f\n',hmd2.y10);
+    fprintf('RK4 (hmax*2): %.4f\n',hmt2.y10);
     
 end
 
